31993be65b99676c134bc14bdf63c8eb80ecdb5a
dinhtoan <55540995+dinhtoan37fc@users.noreply.github.com>
2023-05-31 05:23:51 +0700

    upload

---
+++ src/main/java/org/example/bai1.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class bai1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.printf("Diện tích hình chữ nhật là: %d", a*b);
+    }
+}
