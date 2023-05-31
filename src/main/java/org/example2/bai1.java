a7fefda98c4bb3dafe1c4ff9fa6bfe612a877b74
dinhtoan <55540995+dinhtoan37fc@users.noreply.github.com>
2023-05-31 19:35:58 +0700

    upload

---
+++ src/main/java/org/example2/bai1.java
@@ -0,0 +1,17 @@
+package org.example2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class bai1 {
+    public static void main(String[] args) {
+        Random rd = new Random();
+        int c = rd.nextInt(100);
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Nhập số tự nhiên a = ");
+        int a = sc.nextInt();
+        String b;
+        b = (c == a) ? "đúng":"sai";
+        System.out.println(b);
+    }
+}
---
+++ src/main/java/org/example2/phuongTrinhBac1.java
@@ -0,0 +1,20 @@
+package org.example2;
+
+import java.util.Scanner;
+
+public class phuongTrinhBac1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float a;
+        float b;
+        do{
+            System.out.println("Nhập giá trị: ");
+            System.out.printf("\ta = ");
+            a = scanner.nextFloat();
+        }while (a <= 0);
+        System.out.printf("\tb = ");
+        b = scanner.nextFloat();
+        System.out.printf("");
+
+    }
+        }
