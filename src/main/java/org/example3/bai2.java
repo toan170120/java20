3efdadbf75046416479360762fa1ec100cef75e2
dinhtoan <55540995+dinhtoan37fc@users.noreply.github.com>
2023-06-01 17:42:46 +0700

    upload

---
+++ src/main/java/org/example3/bai2.java
@@ -0,0 +1,26 @@
+package org.example3;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class bai2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        int a;
+        int random = rd.nextInt(100);
+       do{
+           System.out.printf("Nhập giá trị cần đoán: ");
+           a = sc.nextInt();
+           if( a > random){
+               System.out.println("bạn đoán lớn hơn rồi, và cho nhập lại");
+           } else if (a < random) {
+               System.out.println("bạn đoán nhỏ hơn rồi, và cho nhập lại");
+           }
+           else {
+               System.out.println("bạn đoán trúng rồi ");
+           }
+       }while (a != random);
+
+    }
+}
