0baa4be1ae463e7a392c67c91533fcd14e7d3dd8
dinhtoan <55540995+dinhtoan37fc@users.noreply.github.com>
2023-06-04 16:20:48 +0700

    upload

--- src/main/java/org/example2/bai1.java
+++ src/main/java/org/example2/bai1.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 public class bai1 {
     public static void main(String[] args) {
         Random rd = new Random();
-        int c = rd.nextInt(100);
+        int randomNumber = rd.nextInt(100);
         Scanner sc = new Scanner(System.in);
         System.out.printf("Nhập số tự nhiên a = ");
         int a = sc.nextInt();
         String b;
-        b = (c == a) ? "Bạn đã đoán đúng":"Bạn đã đoán sai";
+        b = (randomNumber == a) ? "Bạn đã đoán đúng":"Bạn đã đoán sai";
         System.out.println(b);
 
     }
