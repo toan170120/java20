56d1c8b7ef312d98a623c888e682553c6c589fb0
dinhtoan <55540995+dinhtoan37fc@users.noreply.github.com>
2023-06-09 21:09:17 +0700

    upload

--- src/main/java/org/example6/Bai1.java
+++ src/main/java/org/example6/Bai1.java
@@ -77,7 +77,7 @@ public class Bai1 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhập giá trị: ");
         int a = sc.nextInt();
-            int[][] pascal = new int[a][a];
+            int[][] pascal = new int[100][100];
             for (int i = 0; i < a; i++) {
                 for (int j = 0; j <= i; j++) {
                     if (j == 0 || j == i) {
@@ -87,12 +87,25 @@ public class Bai1 {
                     }
                 }
             }
-            for (int i = 0; i < a; i++) {
-                for (int j = 0; j <= i; j++) {
+//            for (int i = 0; i < a; i++) {
+//                for (int j = 0; j <= i; j++) {
+//                    System.out.print(pascal[i][j] + " ");
+//                }
+//                System.out.println();
+//            }
+
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < a - i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= 2 * i; j++) {
+                if (pascal[i][j] != 0) {
                     System.out.print(pascal[i][j] + " ");
                 }
-                System.out.println();
             }
+            System.out.println();
+        }
+
         }
 }
 
