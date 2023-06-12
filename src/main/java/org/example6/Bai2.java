b07628d799cae790dd502402f01baf5d87125bd4
dinhtoan <55540995+dinhtoan37fc@users.noreply.github.com>
2023-06-12 19:44:59 +0700

    upload

--- src/main/java/org/example6/Bai2.java
+++ src/main/java/org/example6/Bai2.java
@@ -203,24 +203,30 @@ public class Bai2 {
     //Bài 5.
     public static void trungBinhCong() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Nhập số lượng mạng: ");
+        System.out.print("Nhập số phần tử của dãy: ");
         int n = sc.nextInt();
         int[] a = new int[n];
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             System.out.printf("a[%d] = ", i);
             a[i] = sc.nextInt();
         }
 
-        System.out.println("Nhập khoảng để xử lý: ");
-        System.out.print("b = ");
+        System.out.print("Nhập giá trị b: ");
         int b = sc.nextInt();
-        System.out.print("c = ");
-        int c =sc.nextInt();
-        float sum = 0;
-        for(int i = b; i <=c; i++){
-            sum += a[i];
+        System.out.print("Nhập giá trị c: ");
+        int c = sc.nextInt();
+
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] >= b && a[i] <= c) {
+                sum += a[i];
+                count++;
+            }
         }
-        System.out.printf("Trung binh cộng đoạn trong mảng từ %d đến %d là %f", b, c, (sum / (c- b +1)));
+        double tB = (double) sum / count;
+
+        System.out.printf("Trung bình cộng các phần tử trong đoạn %d và %d là %f: ", b, c, tB);
     }
 
 //bài 6. check mảng đối xứng
@@ -438,8 +444,5 @@ public class Bai2 {
         }
 
         System.out.println("tổng chéo: " + sum);
-
-
-
     }
 }
